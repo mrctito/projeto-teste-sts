07d5483e0751d30fe813279a8c6f4c9a01c30db0
Marcos Tito de Pardo Maques <mrctito@gmail.com>
2018-06-22 15:11:36 -0300

    Alteração no arquivo principal

--- src/main/java/com/mrctito/projetotestests/ProjetoTesteStsApplication.java
+++ src/main/java/com/mrctito/projetotestests/ProjetoTesteStsApplication.java
@@ -8,5 +8,7 @@ public class ProjetoTesteStsApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ProjetoTesteStsApplication.class, args);
+		
+		System.out.println("Teste!");
 	}
 }
