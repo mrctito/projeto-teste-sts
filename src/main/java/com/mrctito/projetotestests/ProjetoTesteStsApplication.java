65db17d5b7a2013f20119f71d51916cfb9754e48
Marcos Tito de Pardo Maques <mrctito@gmail.com>
2018-06-22 15:01:58 -0300

    Versão inicial do projeto

---
+++ src/main/java/com/mrctito/projetotestests/ProjetoTesteStsApplication.java
@@ -0,0 +1,12 @@
+package com.mrctito.projetotestests;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoTesteStsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoTesteStsApplication.class, args);
+	}
+}
